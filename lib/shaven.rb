cb37d80278a389566b3b5bb1d257d518bcf2595a
Chris Kowalik <chris@nu7hat.ch>
2011-06-13 00:46:18 -0300

    some conditionals for hash ext loading

--- lib/shaven.rb
+++ lib/shaven.rb
@@ -1,6 +1,9 @@
 require "nokogiri"
 
-require 'shaven/core_ext/hash'
+unless {}.respond_to?(:stringify_keys)
+  require 'shaven/core_ext/hash'
+end
+
 require 'shaven/core_ext/object'
 require 'shaven/nokogiri_ext/node'
 require 'shaven/presenter'
